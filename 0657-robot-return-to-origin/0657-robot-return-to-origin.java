c883487c206eb5f7f0656d181ace05c426f4d39c
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-11-11 00:35:32 +0530

    Time: 10 ms (7.78%), Space: 45.3 MB (8.58%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0, y=0;
+        for(int i=0; i<moves.length(); i++){
+            if(moves.charAt(i)=='U') y++;
+            if(moves.charAt(i)=='D') y--;
+            if(moves.charAt(i)=='L') x--;
+            if(moves.charAt(i)=='R') x++;
+        }
+        return x==0 && y==0;
+    }
+}
