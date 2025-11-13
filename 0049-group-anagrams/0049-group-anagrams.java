fcdbd77e91be3d0f11756b4e962eedcb86dc7943
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-11-14 00:29:29 +0530

    Time: 6 ms (98.53%), Space: 49.8 MB (7.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,39 +1,13 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        int[] arr = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] != 0)
-                return false;
-        }
-        return true;
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-        List<List<String>> ans = new ArrayList<>();
-        boolean[] used = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (used[i])
-                continue;
-            List<String> list = new ArrayList<>();
-            list.add(strs[i]);
-            used[i] = true;
-
-            for (int j = i + 1; j < n; j++) {
-                if (!used[j] && isAnagram(strs[i], strs[j])) {
-                    list.add(strs[j]);
-                    used[j] = true;
-                }
-            }
-            Collections.sort(list);
-            ans.add(list);
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
         }
-        return ans;
+        return new ArrayList<>(ans.values());        
     }
 }
