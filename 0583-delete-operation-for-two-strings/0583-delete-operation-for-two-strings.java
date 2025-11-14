21239651b89777bb306232503a104212db210b88
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-11-15 00:09:22 +0530

    Time: 6 ms (92.93%), Space: 46.3 MB (12.62%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,38 +1,11 @@
-class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int [][] seen = new int[n+1][m+1];
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    seen[i][j]=seen[i-1][j-1]+1;
-                }
-                else{
-                    seen[i][j]=Math.max(seen[i-1][j], seen[i][j-1]);
-                }
-            }
-        }
-        int len = seen[n][m];
-        return n - len + m - len;
-    }
-}
-
 // class Solution {
 //     public int minDistance(String word1, String word2) {
-//         int len = longestCommonSubsequence(word1, word2);
 //         int n = word1.length();
 //         int m = word2.length();
-//         return n - len + m - len;
-//     }
-
-//     public int longestCommonSubsequence(String text1, String text2) {
-//         int n = text1.length();
-//         int m = text2.length();
 //         int [][] seen = new int[n+1][m+1];
 //         for(int i=1; i<=n; i++){
 //             for(int j=1; j<=m; j++){
-//                 if(text1.charAt(i-1)==text2.charAt(j-1)){
+//                 if(word1.charAt(i-1)==word2.charAt(j-1)){
 //                     seen[i][j]=seen[i-1][j-1]+1;
 //                 }
 //                 else{
@@ -40,9 +13,33 @@ class Solution {
 //                 }
 //             }
 //         }
-//         return seen[n][m];
+//         int len = seen[n][m];
+//         return n - len + m - len;
 //     }
 // }
+
+class Solution {
+    public int minDistance(String text1, String text2) {
+        int  n = text1.length();
+        int  m = text2.length();
+        int [][] seen = new int[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                seen[i][j]=-1;
+            }
+        }
+        int len = lcs(text1, text2, n, m, seen);
+        return n - len + m - len;
+
+    }
+    public int lcs(String s1, String s2, int n, int m, int[][] seen){
+        if(m==0 || n==0) return 0;
+        if(seen[n][m] != -1) return seen[n][m];
+        if(s1.charAt(n-1)==s2.charAt(m-1)) return seen[n][m]=1+lcs(s1,s2,n-1,m-1,seen);
+        return seen[n][m]=Math.max(lcs(s1,s2,n-1,m,seen),lcs(s1,s2,n,m-1,seen));
+    }
+}
+
 
 /*
 Example 1
