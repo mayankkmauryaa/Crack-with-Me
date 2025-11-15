0a1f88d94f9f7c6508ef266d14feff499443d0cc
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-11-16 00:26:40 +0530

    Time: 1 ms (86.34%), Space: 47.1 MB (84.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,29 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int size = isConnected.length;
-        boolean[] visitedDFS = new boolean[size];
+        int n = isConnected.length;
+        boolean[] vis = new boolean[n];
         int count = 0;
-        int start_idx = 0;
-        for (int i = start_idx; i < size; i++) {
-            if (!visitedDFS[i]) {
-                List<Integer> dfslist = new ArrayList<>();
-                dfs_graph(i, isConnected, visitedDFS, dfslist);
-                
-                System.out.println("DFS traversal from node " + i + ": " + dfslist);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
                 count++;
+                dfs(i, isConnected, vis);
             }
         }
-        
-        System.out.println("Number of connected components: " + count);
         return count;
     }
 
-    private static void dfs_graph(int node, int [][] isConnected, boolean[] visited, List<Integer> dfs) {
-        dfs.add(node);
-        visited[node] = true;
-        for (int i=0;i<isConnected.length; i++) {
-            if (isConnected[node][i]==1 && !visited[i]) {
-                dfs_graph(i, isConnected, visited, dfs);
+    private void dfs(int idx, int[][] mat, boolean[] vis) {
+        vis[idx] = true;
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i] && mat[idx][i] == 1) {
+                dfs(i, mat, vis);
             }
         }
     }
