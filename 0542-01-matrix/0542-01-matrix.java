b4c0fecbaf1a4736ccff14e6504b19b4a9816286
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-11-18 23:30:52 +0530

    Time: 16 ms (25.16%), Space: 49.6 MB (6.1%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
@@ -36,5 +36,46 @@ class Solution {
             }
         }
         return mat; // data changes here if not wanted to change data use another matrix
+    }
+}
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        boolean vis[][] = new boolean[n][m];
+        int dist[][] = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    q.add(new int[] { i, j ,0});
+                    dist[i][j] = 0;
+                    vis[i][j] = true;
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] pair = q.poll();
+            int idxi = pair[0];
+            int idxj = pair[1];
+            int steps = pair[2];
+            dist[idxi][idxj] = steps;
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    if (Math.abs(i + j) == 1) {
+                        int ni = idxi + i;
+                        int nj = idxj + j;
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
+                            if (!vis[ni][nj]) {
+                                vis[ni][nj] = true;
+                                q.add(new int[] { ni, nj , steps+1});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dist;
     }
 }
