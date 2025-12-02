910090b06faea428b3434f92bf75e0d7b049b63f
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-03 02:34:50 +0530

    Time: 16 ms (71.92%), Space: 74.3 MB (7.08%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,5 +1,32 @@
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        for (int bat : batteries)
+            sum += bat;
+        long low = 0;
+        long high = sum / n;
+        while (low < high) {
+            long mid = low + (high - low + 1) / 2;
+            if (canCharge(batteries, mid, n)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean canCharge(int arr[], long x, int n) {
+        long total = 0;
+        for (int val : arr) {
+            total += Math.min(val, x);
+            if (total >= x * n)
+                return true;
+        }
+        return total >= x * n;
+    }
+
+    public long maxRunTime1(int n, int[] batteries) {
         long total = 0;
         for (int bat : batteries)
             total += bat;
