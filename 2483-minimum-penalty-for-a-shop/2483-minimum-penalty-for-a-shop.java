2e1408f92d049c370179b021eb18f37c9fb4e3e7
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-26 14:55:09 +0530

    Time: 7 ms (98.28%), Space: 47.4 MB (10.69%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -2,21 +2,19 @@ class Solution {
     public int bestClosingTime(String customers) {
         int n = customers.length();
         int pen = 0;
-        for (char c : customers.toCharArray()) {
-            if (c == 'Y')
-                pen++;
-        }
 
-        int minpen = pen;
+        int minpen = 0;
         int bestHour = 0;
 
         for (int i = 0; i < n; i++) {
-            if (customers.charAt(i) == 'Y') pen--;
-            else pen++;
-            if (pen < minpen) {
-                minpen = pen;
-                bestHour = i + 1;
-            }
+            if (customers.charAt(i) == 'Y') {
+                pen--;
+                if (pen < minpen) {
+                    minpen = pen;
+                    bestHour = i + 1;
+                }
+            } else
+                pen++;
         }
 
         return bestHour;
